6173720d5cb559c22cd20476f8260725b31b5625
Michael Floering <michaelfloering@gmail.com>
2017-08-20 13:17:59 -0700

    (setup_logging() should be idempotent)
    ---------------
    & SQUASHED:
    ---------------
    
    Fix duplicate logs in default logging config
    
    needed propagate:False for named logger
    
    Squash me - fix logs

--- presswork/logging/__init__.py
+++ presswork/logging/__init__.py
@@ -6,24 +6,26 @@ import yaml
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
+_PRESSWORK_LOGGING_HAS_BEEN_SET_UP = False
 
-def setup_logging(config_path=os.path.join(HERE, "logging.yaml"), config=None):
+def setup_logging(path_to_logging_yaml=os.path.join(HERE, "logging.yaml")):
     """Setup logging configuration
     """
-    logging.basicConfig()
+    global _PRESSWORK_LOGGING_HAS_BEEN_SET_UP
+    if _PRESSWORK_LOGGING_HAS_BEEN_SET_UP:
+        logger = logging.getLogger('presswork')
+        logger.warning('setup_logging() has already been called! short-circuiting and returning with no changes.')
+        return
 
-    error_message = 'please supply (only) one of: config (dict), config_path (path to YAML)'
-    if config and config_path:
-        raise ValueError(error_message)
-
-    if config:
-        pass
-    elif config_path:
-        with open(config_path, 'r') as f:
+    if path_to_logging_yaml:
+        with open(path_to_logging_yaml, 'r') as f:
             config = yaml.load(f)
     else:
-        raise ValueError(error_message)
+        raise ValueError("config_path needs to be a path to a YAML logging config. "
+                         "see logging.yaml in this directory for a starter")
 
     logging.config.dictConfig(config)
+
+    _PRESSWORK_LOGGING_HAS_BEEN_SET_UP = True
 
     return logging.getLogger('presswork')
