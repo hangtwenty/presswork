019443f7c0bcdcaecef6bf6840eb69cd1745e6e3
Michael Floering <michaelfloering@gmail.com>
2017-08-19 19:40:50 -0700

    Add smoketest for Flask app, & bit of cleanup.
    just smoketest so far, more to come

--- presswork/flask_app/app.py
+++ presswork/flask_app/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-""" Little Flask server to expose the MarkovChainTextMaker
-
+""" Little Flask app FOR LOCAL USE ONLY, for rapidly playing around with text generation.
 """
+import logging
 import uuid
 
 from flask import Flask, render_template
@@ -10,47 +10,65 @@ from flask.ext.wtf.csrf import CsrfProtect
 from wtforms import validators, StringField, \
     IntegerField
 
-from presswork.text_maker.impl.pymarkovchain_with_nltk import PyMarkovChainWithNLTK
+# TODO(hangtwenty) swap this out for general TextMaker/subclasses, and in the form allow changing which one is used
+from presswork.text_maker._pymarkovchain_fork import PyMarkovChainWithNLTK
 
 app = Flask(__name__)
 csrf = CsrfProtect(app=app)
 app.config['SECRET_KEY'] = str(uuid.uuid4())
 
+logger = logging.getLogger('presswork')
 
 class MarkovChainTextMakerForm(Form):
     text = StringField(
-        'Source text',
-        [validators.InputRequired(), validators.Length(max=1000000)])
+        'Input text',
+        [
+            validators.InputRequired(),
+            validators.Length(max=1000000) # XXX: this limit was picked arbitrarily, not by observation nor testing
+        ]
+    )
 
-    # TODO(hangtwenty) (someday/maybe) if extending or adding a markov impl that supports multi models w/ weighting,
-    # it'd be nice to be able to put in multiple separated sources of different weights - a "+=" button on the form,
-    # adding more boxes, which are loaded into more models. nice-to-have, or maybe not worth it
-
-    window = IntegerField(
-        "Window size AKA state size (increase for more 'rigid' modeling of source text)",
+    state_size = IntegerField(
+        "Window size AKA state size AKA N-Gram Size (increase for more 'rigid' modeling of input text)",
         [validators.NumberRange(min=1, max=9)],
         default=2, )
 
     count_of_sentences_to_make = IntegerField(
         "Number of sentences to generate", [validators.NumberRange(min=1, max=3000)], default=25, )
 
+    # TODO(hangtwenty) instead of always just join_with=<space>, should give option to specify join characters,
+    # max length 100 ... " " is valid ... "\n" gets replaced by newline... unicode gets rejected
+
 
 @app.route("/", methods=['GET', 'POST', ])
 def markov():
     form = MarkovChainTextMakerForm()
     if form.validate_on_submit():
-        text_maker = PyMarkovChainWithNLTK(db_file_path=None, window=form.window.data)
-        text_maker.database_init(form.text.data)
-        text_made = text_maker.make_sentences(form.count_of_sentences_to_make.data)
-        text_made_title = text_maker.make_sentence().strip('.')
+        logger.info('received valid form submission')
 
-        for field_name in ('text', 'window', 'count_of_sentences_to_make'):
+        state_size = form.state_size.data
+        input_text = form.text.data
+        count_of_sentences_to_make = form.count_of_sentences_to_make.data
+
+        # keep last submission in the text fields (supports 'accumulating' workflow)
+        # (there is probably a cleaner way to do this with WTForms)
+        for field_name in ('text', 'state_size', 'count_of_sentences_to_make'):
             field = getattr(form, field_name)
-            # if submitted, keep last submission in the text fields (supports 'accumulating' workflow)
-            field.default = field.data
+            if field.data:
+                field.default = field.data
+
+        logger.info('notable form parameters: state_size={}, count_of_sentences_to_make={} '
+                    '(input text shown at DEBUG level)'.format(state_size, count_of_sentences_to_make))
+        logger.debug('input_text=\n{}'.format(input_text))
+
+        text_maker = PyMarkovChainWithNLTK(db_file_path=None, window=state_size)
+        text_maker.database_init(input_text)
+        text_made = text_maker.make_sentences(count_of_sentences_to_make)
+        text_made_title = text_maker.make_sentence().strip('.')
 
         return render_template(
             'index.html', form=form, text_made=text_made, text_made_title=text_made_title)
+
     return render_template('index.html', form=form)
 
 
---
+++ tests/flask_app/test_app.py
@@ -0,0 +1,14 @@
+""" minimal Flask app (for now) calls for a minimal test suite (for now)
+"""
+import pytest
+
+
+@pytest.mark.usefixtures("testapp")
+class TestSmokeTests(object):
+
+    def test_index(self, testapp):
+        """ Tests if the index page loads """
+
+        response = testapp.get('/')
+        assert response.status_code == 200
+
